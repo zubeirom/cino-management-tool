ddeb1359c55babe036bac7425c58568adcdb72dc
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2020-02-25 00:03:10 +0100

    Render new packet page and setup props

--- app/controllers/packets/new.js
+++ app/controllers/packets/new.js
@@ -1,4 +1,18 @@
 import Controller from '@ember/controller';
+import { set } from '@ember/object';
 
 export default Controller.extend({
+    types: ["Standard", "Bundle", "Sale"],
+    properties: [],
+
+    actions: {
+        addKeyword(prop) {
+            this.properties.pushObject(prop);
+            set(this, "property", "")
+        },
+
+        deleteKeyword(prop) {
+            this.properties.removeObject(prop);
+        }
+    }
 });
